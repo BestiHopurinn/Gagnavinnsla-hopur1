4f7976b60a21179e618e0582c0e8dd973013e799
gudrung11 <gudrung11@ru.is>
2015-12-01 16:45:34 +0000

    Update stora.py

--- stora.py
+++ stora.py
@@ -1,6 +1,10 @@
 import csv
 
-with open('nafnid a skranni') as csvfile
-  thereader = csv.reader(csvfile)
-  for row in thereader
-    print(row)
+#vantar að biðja um skraar nafn
+
+# svona opnar maður csv skrá og prentar út
+with open('SAM02011.csv') as csvfile:
+  thereader = csv.reader(csvfile, delimiter=';')
+  for row in thereader:
+    print(len(row))
+    print(row)i
